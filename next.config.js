08a50d2368390ee92f6b2813e83533cd00403221
adrianrivald <adrianrivaldy@gmail.com>
2024-12-26 08:47:52 +0700

    rollback nextconfig

--- next.config.js
+++ next.config.js
@@ -2,15 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: process.env.NEXT_PUBLIC_HOST_URL,
-        permanent: false,
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
