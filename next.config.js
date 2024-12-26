32f2c15f8b0a97e9292065d9dbebc0c86282c24c
adrianrivald <adrianrivaldy@gmail.com>
2024-12-26 08:25:29 +0700

    fix nextconfig

--- next.config.js
+++ next.config.js
@@ -2,6 +2,27 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: process.env.NEXT_PUBLIC_HOST_URL,
+        permanent: false,
+      },
+      {
+        source: "/:slug",
+        destination: process.env.NEXT_PUBLIC_HOST_URL,
+        has: [
+          {
+            type: "query",
+            key: "redir",
+            value: "true",
+          },
+        ],
+        permanent: false,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
